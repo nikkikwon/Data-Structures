1efe31c959df2f3d3fca101d908e19ce71ee3205
hyojung2 <hyojung2@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-09-25 08:31:01 +0000

    mp2 submission

--- mp2/main.cpp
+++ mp2/main.cpp
@@ -1,3 +1,29 @@
+#include"Image.h"
+#include"StickerSheet.h"
+
 int main() {
+  Image alma;
+  alma.readFromFile("tests/alma.png");
+  
+  Image i;
+  i.readFromFile("tests/i.png");
+
+  StickerSheet sheet(alma, 5);
+  sheet.addSticker(i, 20, 200);
+
+  StickerSheet lol(alma, 5);
+  sheet.addSticker(i, 50, 100);
+
+  StickerSheet omg(alma, 5);
+  sheet.addSticker(i, 90, 10);
+
+  Image myImage;
+  myImage = sheet.render();
+  myImage.writeToFile("myImage.png");
+
   return 0;
 }
+
+
+
+
